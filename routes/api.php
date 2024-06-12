801cf589ef70b8c361f35362780b41b12cf0cc03
sreynuch17 <sreynuch.thoeun@student.passerellesnumeriques.org>
2024-06-12 19:52:45 +0700

    #3 crud_post

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
+
+Route::get('/post/list',[PostController::class,'index'])->name('post.list');
+Route::post('/post/create',[PostController::class,'store'])->name('post.create');
+Route::get('/post/show/{id}',[PostController::class,'show'])->name('post.show');
+Route::put('/post/update/{id}',[PostController::class,'update'])->name('post.update');
+Route::delete('/post/delete/{id}',[PostController::class,'destroy'])->name('post.destroy');
